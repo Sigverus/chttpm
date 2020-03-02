22ae5fa6378a10eb6596f07c3d8c5b227a35daf3
Kirlim <phenomenous@gmail.com>
2020-03-01 23:31:25 -0300

    Simplifies the main by using a single lambda to handle all HTTP Methods.

--- src/chttpm/main.cpp
+++ src/chttpm/main.cpp
@@ -13,62 +13,22 @@ int main(int argc, char* argv[])
 
 	httplib::Server server{};
 
-	server.Delete(
-		R"((.*))",
-		[&](const httplib::Request& request, httplib::Response& response)
-		{
-			auto url = request.matches[1];
-			response.set_content(url.str() + "\n[Delete] Hello World!", "text/plain");
-		}
-	);
+	auto handlerFunc = [&](const httplib::Request& request, httplib::Response& response)
+	{
+		auto url = request.matches[1];
+		char buffer[512]; // Not really safe. Will be removed soon, when we removing the hello world.
+		sprintf(buffer, "%s %s\nHello World!\n", request.method.c_str(), request.target.c_str());
+		response.set_content(buffer, "text/plain");
 
+		scriptingService.ExecuteModule("MyModule");
+	};
 
-	server.Get(
-		R"((.*))",
-		[&](const httplib::Request& request, httplib::Response& response)
-		{
-			auto url = request.matches[1];
-			response.set_content(url.str() + "\n[Get] Hello World! " + request.target, "text/plain");
-
-			scriptingService.ExecuteModule("MyModule");
-		}
-	);
-
-	server.Options(
-		R"((.*))",
-		[&](const httplib::Request& request, httplib::Response& response)
-		{
-			auto url = request.matches[1];
-			response.set_content(url.str() + "\n[Options] Hello World!", "text/plain");
-		}
-	);
-
-	server.Patch(
-		R"((.*))",
-		[&](const httplib::Request& request, httplib::Response& response)
-		{
-			auto url = request.matches[1];
-			response.set_content(url.str() + "\n[Patch] Hello World!", "text/plain");
-		}
-	);
-
-	server.Post(
-		R"((.*))",
-		[&](const httplib::Request& request, httplib::Response& response)
-		{
-			auto url = request.matches[1];
-			response.set_content(url.str() + "\n[Post] Hello World!\n" + request.body, "text/plain");
-		}
-	);
-
-	server.Put(
-		R"((.*))",
-		[&](const httplib::Request& request, httplib::Response& response)
-		{
-			auto url = request.matches[1];
-			response.set_content(url.str() + "\n[Put] Hello World!", "text/plain");
-		}
-	);
+	server.Delete(R"((.*))", handlerFunc);
+	server.Get(R"((.*))", handlerFunc);
+	server.Options(R"((.*))", handlerFunc);
+	server.Patch(R"((.*))", handlerFunc);
+	server.Post(R"((.*))", handlerFunc);
+	server.Put(R"((.*))", handlerFunc);
 
 	server.listen("0.0.0.0", 8080);
 	return 0;
